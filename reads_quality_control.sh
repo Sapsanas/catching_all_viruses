0024ef67123cdeafc059e76eaf6402677f07e50e
Sana <34066681+Sapsanas@users.noreply.github.com>
2021-12-03 17:28:32 +0100

    Update reads_quality_control.sh

--- reads_quality_control.sh
+++ reads_quality_control.sh
@@ -12,57 +12,58 @@ echo "SAMPLE_ID=${SAMPLE_ID}"
 export PATH=$PATH:/home/umcg-sgarmaeva/.local
 
 ##### MODULES #####
-#module load FastQC
-#module load Java/11-LTS
+module load FastQC
+module load Java/11-LTS
 module load Python/3.7.4-GCCcore-7.3.0-bare
 module load Bowtie2
 
-# Examining the raw sequencing data
+## Examining the raw sequencing data
 
-#fastqc ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_1.fq.gz
-#fastqc ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_2.fq.gz
+fastqc ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_1.fq.gz
+fastqc ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_2.fq.gz
 
-#mv ../SAMPLES/${SAMPLE_ID}/*fastqc.* ../OUTPUT/FastQC_reports/before_QC/ 
+mv ../SAMPLES/${SAMPLE_ID}/*fastqc.* ../OUTPUT/FastQC_reports/before_QC/ 
 
-# adapter trimming 
+## adapter trimming 
 
-#mkdir ../SAMPLES/${SAMPLE_ID}/raw_reads
-#mkdir ../SAMPLES/${SAMPLE_ID}/clean_reads
+mkdir ../SAMPLES/${SAMPLE_ID}/raw_reads
+mkdir ../SAMPLES/${SAMPLE_ID}/clean_reads
 
-#/groups/umcg-llnext/tmp01/umcg-agulyaeva/NEXT_ASSEMBLY/SOFTWARE/bbmap/bbduk.sh \
-#	in1=../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_1.fq.gz \
-#	in2=../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_2.fq.gz \
-#	out1=../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_1.fq.gz \
-#	out2=../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_2.fq.gz \
-#	ref=/groups/umcg-llnext/tmp01/umcg-agulyaeva/NEXT_ASSEMBLY/next_cohort_adapters.fa \
-#	ktrim=r k=23 mink=11 hdist=1 tpe tbo \
-#	threads=${SLURM_CPUS_PER_TASK} \
-#	-Xmx16g
+/groups/umcg-llnext/tmp01/umcg-agulyaeva/NEXT_ASSEMBLY/SOFTWARE/bbmap/bbduk.sh \
+	in1=../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_1.fq.gz \
+	in2=../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_2.fq.gz \
+	out1=../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_1.fq.gz \
+	out2=../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_2.fq.gz \
+	ref=/groups/umcg-llnext/tmp01/umcg-agulyaeva/NEXT_ASSEMBLY/next_cohort_adapters.fa \
+	ktrim=r k=23 mink=11 hdist=1 tpe tbo \
+	threads=${SLURM_CPUS_PER_TASK} \
+	-Xmx16g
 	
-# quality trimming & removal of human reads
+## quality trimming & removal of human reads
 
-#kneaddata \
-#	--input ../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_1.fq.gz \
-#	--input ../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_2.fq.gz \
-#	--bypass-trf \
-#	--threads ${SLURM_CPUS_PER_TASK} \
-#	--processes 6 \
-#	-db /groups/umcg-llnext/tmp01/tmp_tools/hg37dec_v0.1 \
-#	--sequencer-source none \
-#	--output-prefix ${SAMPLE_ID}_kneaddata \
-#	--output ../SAMPLES/${SAMPLE_ID}/raw_reads/ \
-#	--log ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}.log \
+kneaddata \
+	--input ../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_1.fq.gz \
+	--input ../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_AdaptTr_QualTr_2.fq.gz \
+	--bypass-trf \
+	--threads ${SLURM_CPUS_PER_TASK} \
+	--processes 6 \
+	-db /groups/umcg-llnext/tmp01/tmp_tools/hg37dec_v0.1 \
+	--sequencer-source none \
+	--output-prefix ${SAMPLE_ID}_kneaddata \
+	--output ../SAMPLES/${SAMPLE_ID}/raw_reads/ \
+	--log ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}.log \
 
-#mv ../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_kneaddata_paired_*.fastq ../SAMPLES/${SAMPLE_ID}/clean_reads
-#rm -r ../SAMPLES/${SAMPLE_ID}/raw_reads
+mv ../SAMPLES/${SAMPLE_ID}/raw_reads/${SAMPLE_ID}_kneaddata_paired_*.fastq ../SAMPLES/${SAMPLE_ID}/clean_reads
+rm -r ../SAMPLES/${SAMPLE_ID}/raw_reads
 
-# quality control assessment
-#fastqc ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}_kneaddata_paired_1.fastq
-#fastqc ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}_kneaddata_paired_2.fastq
+## quality control assessment
 
-#mv ../SAMPLES/${SAMPLE_ID}/clean_reads/*fastqc.* ../OUTPUT/FastQC_reports/post_QC 
+fastqc ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}_kneaddata_paired_1.fastq
+fastqc ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}_kneaddata_paired_2.fastq
 
-# bacterial contamination assessment
+mv ../SAMPLES/${SAMPLE_ID}/clean_reads/*fastqc.* ../OUTPUT/FastQC_reports/post_QC 
+
+## bacterial contamination assessment
 
 bowtie2 \
 	-x /groups/umcg-lld/tmp01/other-users/umcg-sgarmaeva/databases/cpn60/cpn60db_20211203/cpn60db \
