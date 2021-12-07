9784cfb695ed8facc60d0c3671b70a4dfe19a4d1
Sana <34066681+Sapsanas@users.noreply.github.com>
2021-12-07 16:06:13 +0100

    Update assembly.sh
    Added option for relaunching the assembly in case it stopped at the certain checkpoint (like K55 started). Added renaming of contigs for future parsing.

--- assembly.sh
+++ assembly.sh
@@ -9,28 +9,36 @@
 SAMPLE_ID=$1
 echo "SAMPLE_ID=${SAMPLE_ID}"
 
-# per sample assembly
+## per sample assembly
 /groups/umcg-llnext/tmp01/umcg-agulyaeva/NEXT_ASSEMBLY/SOFTWARE/SPAdes-3.15.3-Linux/bin/metaspades.py \
+	#--continue \
 	-1 ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}_kneaddata_paired_1.fastq \
     	-2 ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}_kneaddata_paired_2.fastq \
     	-o ../SAMPLES/${SAMPLE_ID}/metaSPAdes_out \
     	--threads ${SLURM_CPUS_PER_TASK}
 
-# assembly quality assessment
+## assembly quality assessment
 /groups/umcg-llnext/tmp01/umcg-sgarmaeva/SOFTWARE/quast-5.0.2/quast.py \
 	-o ../SAMPLES/${SAMPLE_ID}/quast_out \
 	--min-contig 1000 \
 	--threads ${SLURM_CPUS_PER_TASK} \
 	../SAMPLES/${SAMPLE_ID}/metaSPAdes_out/scaffolds.fasta
 
-# gzipping all the fqs
+## gzipping the clean reads and removing intermediate spades results to save space; filtering and renaming scaffolds.fasta for the ease of downstream analysis
 
 if [ -f ../SAMPLES/${SAMPLE_ID}/metaSPAdes_out/scaffolds.fasta ]
 then 
 	gzip ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}_kneaddata_paired_1.fastq
 	gzip ../SAMPLES/${SAMPLE_ID}/clean_reads/${SAMPLE_ID}_kneaddata_paired_2.fastq
+
 	mv ../SAMPLES/${SAMPLE_ID}/metaSPAdes_out/scaffolds.fasta ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_scaffolds.fasta
 	mv ../SAMPLES/${SAMPLE_ID}/metaSPAdes_out/contigs.fasta ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_contigs.fasta
 	mv ../SAMPLES/${SAMPLE_ID}/metaSPAdes_out/spades.log ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_spades.log
 	rm -rf ../SAMPLES/${SAMPLE_ID}/metaSPAdes_out
+	chmod 666 ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_*
+	
+	/groups/umcg-lld/tmp01/other-users/umcg-sgarmaeva/scripts/filter_contigs.pl 1000 ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_scaffolds.fasta > ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_scaffolds.min1000.fasta
+
+	sed -i 's/>NODE/>'${SAMPLE_ID}'_NODE/g' ../SAMPLES/${SAMPLE_ID}/${SAMPLE_ID}_scaffolds.min1000.fasta 
 fi
+
