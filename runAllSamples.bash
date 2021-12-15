86e6b7dad12b1137bb9544549a78159370737db3
Sana <34066681+Sapsanas@users.noreply.github.com>
2021-12-15 15:38:46 +0100

    Update runAllSamples.bash
    Commented and active lines for running all the samples through different stages

--- runAllSamples.bash
+++ runAllSamples.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 for SAMPLE in $@; do
-        sbatch --output >> ./out/${SAMPLE}.out --error >> ./err/${SAMPLE}.err --job-name ${SAMPLE} reads_quality_control.sh ${SAMPLE} 
+        #sbatch --output ./out/${SAMPLE}.out --error ./err/${SAMPLE}.err --job-name ${SAMPLE} reads_quality_control.sh ${SAMPLE} 
+	#sbatch --output ./out/${SAMPLE}.out --error ./err/${SAMPLE}.err --job-name ${SAMPLE} assembly.sh ${SAMPLE}
+	sbatch --output ./out/${SAMPLE}.out --error ./err/${SAMPLE}.err --job-name ${SAMPLE} continue_assembly.sh ${SAMPLE}
+	#sbatch --output ./out/${SAMPLE}.out --error ./err/${SAMPLE}.err --job-name ${SAMPLE} reads_error_correction.sh ${SAMPLE}
 done
